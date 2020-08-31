d78afa8abaf44548f594231533db14f57a81481d
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-08-30 21:20:55 -0300

    Updates

---
+++ precache-manifest.38859d0ef3dc299acd77c1faeba9d84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dece9f57e61e2643e23abbfd9ca46a94",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "12cbfcb54026930a610f",
+    "url": "/weatherapp/static/css/main.37bfb52e.chunk.css"
+  },
+  {
+    "revision": "83a7d1579a08e61a94ed",
+    "url": "/weatherapp/static/js/2.d65f5adf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.d65f5adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cbfcb54026930a610f",
+    "url": "/weatherapp/static/js/main.efd73483.chunk.js"
+  },
+  {
+    "revision": "920c4f4d9cc02298ad3e",
+    "url": "/weatherapp/static/js/runtime-main.d02b9a6b.js"
+  }
+]);
